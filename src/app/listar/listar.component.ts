121179ee868e10b69c9830c0dfdea1b9a66f3822
gabrielgqueiroz <ggabriel12.gg@gmail.com>
2022-06-04 18:31:47 -0300

    deletar e update

--- src/app/listar/listar.component.ts
+++ src/app/listar/listar.component.ts
@@ -10,6 +10,9 @@ import { WebService } from '../web.service';
 export class ListarComponent implements OnInit {
 
   listaProdutos: Produto[];
+  
+  produto = {_id:"" ,title : "", price: 0.0, description: ""};
+  prodToAtt: Produto;
 
   constructor(private web : WebService) { }
 
@@ -22,5 +25,38 @@ export class ListarComponent implements OnInit {
   ngOnInit(): void {
     this.carregarProdutos();
   }
+
+  deletarProd(produto: Produto): void{
+    this.web.deletarProduto(produto).subscribe(res => {
+      if (res.ok){
+        this.carregarProdutos();
+        alert(res.body.msg);
+      }
+      else {
+        alert("Não foi possivel deletar o produto: " + res.body.msg);
+      }
+    })
+  }
+
+  atualizarProd(): void {
+    console.log(this.produto);
+    this.web.atualizarProduto(this.produto).subscribe(res => {
+      console.log(res);
+      if (res.ok){
+        this.carregarProdutos();
+        alert(res.body.msg);
+      }
+      else{
+        alert("Não foi possivel atualizar o produto: " + res.body.msg);
+      }
+    })
+  }
+
+  changeVar(prod: Produto): void{
+    this.prodToAtt = prod;
+    this.produto = prod;
+    console.log(this.prodToAtt);
+  }
+
 
 }
